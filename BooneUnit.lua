69c4b160ceb4ad49dbd7e6110701407682a8fdae
Boone Grant <boone.the@gmail.com>
2019-01-15 22:33:52 -0800

    Further progress on object model, but closure is dead end
    Not able to resolve closure within the test.
    May have to move functions outside of data objects.
    Moved tests to separate file.

--- BooneUnit.lua
+++ BooneUnit.lua
@@ -5,77 +5,23 @@ function booneUnit:reset ()
 end
 
 function booneUnit:describe ( description, allTests )
-    local feature = {}
-    table.insert( self.features, feature )   
-    feature.description = description
-    feature.tests = {} 
-    feature.numTests = 0
-    function feature:runTests ()
-        allTests()
+    local thisFeature = {}
+    table.insert( self.features, thisFeature )   
+    thisFeature.description = description
+    thisFeature.tests = {} 
+    thisFeature.numTests = 0
+    
+    function thisFeature:runTests ()
+        scope = self
+        return allTests()
     end
-    function feature:test( description, scenario )
-        
+    
+    function thisFeature:test( description, scenario )
+        local thisTest = {}
+        table.insert( self.tests, thisTest )
+        thisTest.testNum = #self.tests 
+        thisTest.description = description
     end
-     
+    
 end
 
-function testBooneUnit()
-    --CodeaUnit.detailed = false
-    local unitMembers = { "features", "numTests", "describe" }
-    local featureMembers = { "description", "tests", "numTests", "runTests", "test" }
-    local featureDesc = "print"
-    _:describe( "booneUnit", function ()
-        _:test( "booneUnit exists", function ()
-            _:expect( booneUnit ).isnt( nil )
-        end )
-        
-        -- init
-        _:test( "set/reset", function ()
-            _:expect( booneUnit.reset ).isnt( nil )
-            _:expect( booneUnit:reset() ).is( nil )
-        end )
-        
-        --members
-        for i, v in ipairs( unitMembers ) do
-            _:test( "booneUnit contains " .. v, function ()
-                _:expect( booneUnit[ v ] ).isnt( nil )
-            end )
-        end
-        
-        -- features is empty
-        _:test( "booneUnit.features is empty", function ()
-            local f = booneUnit.features
-            _:expect( f[1] ).is( nil )
-            _:expect( #f ).is( 0 )
-        end )
-        
-        --call :describe()
-        _:test( "call describe()", function ()
-            _:expect( booneUnit:describe( featureDesc, function() 
-                print("woo-hoo!") end) ).is( nil )
-        end )
-        
-        -- features not empty
-        _:test( "booneUnit.features not empty", function ()
-            local f = booneUnit.features
-            _:expect( #f ).is( 1 )
-            _:expect( f[1] ).isnt( nil )
-        end )
-        
-        -- feature members 
-        for i, v in ipairs( featureMembers ) do
-            _:test( "features[1] has " .. v, function ()
-                local f = booneUnit.features
-                _:expect( f[ 1 ][ v ] ).isnt( nil )
-            end )
-        end
-        
-        -- feature description 
-        _:test( "feature description matches argument", function ()
-            local target = booneUnit.features[1]
-            _:expect( target.description ).is( featureDesc )
-        end )
-        
-        
-    end )
-end
--- Main.lua
+++ Main.lua
@@ -17,3 +17,81 @@ function draw()
     
 end
 
+function testBooneUnit()
+    --CodeaUnit.detailed = false
+    _:describe( "booneUnit", function ()
+        local unitMembers = { "features", "numTests", "describe" }
+        local featureMembers = { "description", "tests", "numTests", "runTests", "test" }
+        local featureDesc = "print"
+        local testDesc = "this is a test"
+        local testFunc = function ()
+            print( "testing!" )
+            return "some results"
+        end
+        
+        _:test( "booneUnit exists", function ()
+            _:expect( booneUnit ).isnt( nil )
+        end )
+        
+        -- init
+        _:test( "set/reset", function ()
+            _:expect( booneUnit.reset ).isnt( nil )
+            _:expect( booneUnit:reset() ).is( nil )
+        end )
+        
+        --members
+        for i, v in ipairs( unitMembers ) do
+            _:test( "booneUnit contains " .. v, function ()
+                _:expect( booneUnit[ v ] ).isnt( nil )
+            end )
+        end
+        
+        -- features is empty
+        _:test( "booneUnit.features is empty", function ()
+            local f = booneUnit.features
+            _:expect( f[1] ).is( nil )
+            _:expect( #f ).is( 0 )
+        end )
+        
+        --call :describe()
+        _:test( "call describe()", function ()
+            _:expect( booneUnit:describe( featureDesc, function() 
+                print("woo-hoo!")
+                self:test( testDesc, testFunc )
+                return( 42 )
+            end) ).is( nil )
+        end )
+        
+        -- features not empty
+        _:test( "booneUnit.features not empty", function ()
+            local f = booneUnit.features
+            _:expect( #f ).is( 1 )
+            _:expect( f[1] ).isnt( nil )
+        end )
+        
+        -- feature members 
+        for i, v in ipairs( featureMembers ) do
+            _:test( "features[1] has " .. v, function ()
+                local f = booneUnit.features
+                _:expect( f[ 1 ][ v ] ).isnt( nil )
+            end )
+        end
+        
+        -- feature description 
+        _:test( "feature description matches argument", function ()
+            local target = booneUnit.features[1]
+            _:expect( target.description ).is( featureDesc )
+        end )
+        
+        -- run feature tests 
+        _:test( "run feature tests", function ()
+            local target = booneUnit.features[1]
+            _:expect( target.runTests() ).is( nil )
+        end )
+        
+        
+        
+        
+    end )
+end
+
