5a2e986cf4bc9cc1d5d0442d20c332952c67aebd
Boone Grant <boone.the@gmail.com>
2019-01-21 20:59:57 -0800

    Rearrange function order in file. No other change

--- BooneUnit.lua
+++ BooneUnit.lua
@@ -34,14 +34,6 @@ end
 function booneUnit:ignore( description, scenario ) 
 end
 
-function booneUnit:before(setup)
-    if self.currentFeature then self.currentFeature.before = setup end
-    -- error message: before and after must be inside a describe: declaration
-end
-function booneUnit:after(teardown)
-    if self.currentFeature then self.currentFeature.after = teardown end
-end
-
 function booneUnit:orphanage()      -- create a home for tests outside of a :define() call
     print( "Oh you poor lost test!" )
     if ( self.aHomeForOrphanTests == nil ) then
@@ -53,55 +45,12 @@ function booneUnit:orphanage()      -- create a home for tests outside of a :def
     return self.aHomeForOrphanTests
 end
 
-
-
--- Feature class --
-booneUnit.newFeature = class()
-function booneUnit.newFeature:init( featureDescription , allFeatureTests )
-    self.description = featureDescription or ""
-    self.tests = {}
-    self.featureTests = allFeatureTests or ( function() end )  -- test for value = function?
+function booneUnit:before(setup)
+    if self.currentFeature then self.currentFeature.before = setup end
+    -- error message: before and after must be inside a describe: declaration
 end
-function booneUnit.newFeature:runTests()
-    print( self:intro() )
-    self.tests = {}
-    self.featureTests()
-    print( self:results() )
-end
-function booneUnit.newFeature:intro()
-    return string.format( "Feature: %s \ntests:", self.description )
-end
-function booneUnit.newFeature:results()
-    return string.format( "Feature: %s \nResults go here", self.description )
-    -- do some tallying
-end
-function booneUnit.newFeature.before() end
-function booneUnit.newFeature.after() end
-
--- Test class --
-booneUnit.newTest = class()
-function booneUnit.newTest:init( parent, testDescription, scenario )
-    self.feature = parent
-    self.description = testDescription or ""
-    self.test = scenario or ( function() end )
-end
-function booneUnit.newTest:run()
-    local status, err = pcall(self.test)
-    if err then
-        --self.failures = self.failures + 1
-        print(string.format("%d: %s -- %s \n--FAIL", 69, self.description, err))
-        self:registerResult( "fail", err )
-        --store result
-    end
-end
-function booneUnit.newTest:registerResult( outcome, expected )
-    print( string.format( "Result: %s -- %s", outcome, expected ) )
-end
-function booneUnit.newTest:report()
-    
-end
-function booneUnit.newTest:passed()
-    
+function booneUnit:after(teardown)
+    if self.currentFeature then self.currentFeature.after = teardown end
 end
 
 function booneUnit:expect( conditional )
@@ -159,3 +108,54 @@ function booneUnit:expect( conditional )
         throws = throws
     }
 end
+
+
+-- Feature class --
+booneUnit.newFeature = class()
+function booneUnit.newFeature:init( featureDescription , allFeatureTests )
+    self.description = featureDescription or ""
+    self.tests = {}
+    self.featureTests = allFeatureTests or ( function() end )  -- test for value = function?
+end
+function booneUnit.newFeature:runTests()
+    print( self:intro() )
+    self.tests = {}
+    self.featureTests()
+    print( self:results() )
+end
+function booneUnit.newFeature:intro()
+    return string.format( "Feature: %s \ntests:", self.description )
+end
+function booneUnit.newFeature:results()
+    return string.format( "Feature: %s \nResults go here", self.description )
+    -- do some tallying
+end
+function booneUnit.newFeature.before() end
+function booneUnit.newFeature.after() end
+
+-- Test class --
+booneUnit.newTest = class()
+function booneUnit.newTest:init( parent, testDescription, scenario )
+    self.feature = parent
+    self.description = testDescription or ""
+    self.test = scenario or ( function() end )
+end
+function booneUnit.newTest:run()
+    local status, err = pcall(self.test)
+    if err then
+        --self.failures = self.failures + 1
+        print(string.format("%d: %s -- %s \n--FAIL", 69, self.description, err))
+        self:registerResult( "fail", err )
+        --store result
+    end
+end
+function booneUnit.newTest:registerResult( outcome, expected )
+    print( string.format( "Result: %s -- %s", outcome, expected ) )
+end
+function booneUnit.newTest:report()
+    
+end
+function booneUnit.newTest:passed()
+    
+end
+
