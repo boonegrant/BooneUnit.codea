e764b013892151c99b3b9e299b69606f954b8e5b
Boone Grant <boone.the@gmail.com>
2019-01-23 13:48:05 -0800

    Rearranged test code
    Stub test: registerResult()

--- booneUnitTests.lua
+++ booneUnitTests.lua
@@ -83,40 +83,89 @@ function bestBooneUnit()
         end )
     end )
     
-    -- booneUnit.test()
-    _:describe( "booneUnit.test() returns a test object", function()
-        _:test( "booneUnit.test() exists", function()
-            _:expect( type( booneUnit.test ) ).is( "function" )
-        end )
-        -- test() returns a table
-        _:test( "booneUnit:test( description_string, function ) returns a table", function()
-            local testDesc = "Gingerbread Man"
-            local emptyTestFunc = function() end 
-            _:expect( type( booneUnit:test( testDesc, emptyTestFunc ) ) ).is( "table" )
-        end )
-        -- test() without arguments returns a table
-        _:test( "booneUnit:test() without arguments returns a table", function()
-            _:expect( type( booneUnit:test() ) ).is( "table" )
-        end )
-        -- properties of that table
-        local testDesc = "Gingerbread Man"
-        local emptyTestFunc = function() end 
-        local testTable = booneUnit:test( testDesc, emptyTestFunc )
-        local testTableProperties = { passed = "function",
-                                      report = "function",
-                                      results = "table" }
-        memberTypeTest("booneUnit:test produces table", testTable, testTableProperties )
-        
-        -- length of table "results"
-        _:test( "length of table results is 0 with empty test", function()
-            local testTable = booneUnit:test( testDesc, emptyTestFunc )
-            _:expect( #testTable.results ).is( 0 )
-        end )
-        
-    end )
 end
 
-function testBooneUnitExpect()
+function bestMoonUnitFeature() 
+    -- Feature Creation --
+    _:describe( "booneUnit creates Features", function ()
+        
+        _:before( function() end )
+        
+        -- booneUnit member test 
+        local unitMembers = { features = "table", 
+                              currentFeature = "nil", 
+                              currentTest = "nil", 
+                              describe = "function",
+                              test = "function",
+                              reset = "function",
+                              bork = "number",
+                              beck = "number" }
+        memberTypeTest( "booneUnit", booneUnit, unitMembers )
+        
+        -- features is empty
+        _:test( "booneUnit.features is empty", function ()
+            local f = booneUnit.features
+            _:expect( f[1] ).is( nil )
+            _:expect( #f ).is( 0 )
+        end )
+        
+        _:test( "booneUnit.currentFeature is nil", function ()
+            local f = booneUnit
+            _:expect( f.currentFeature ).is( nil )
+        end )
+        
+        --call :describe()
+        _:test( "call describe(), returns nil", function ()
+            _:expect( booneUnit:describe( featureDesc, featureFunc ) ).isnt( nil )
+        end )
+        
+        -- features not empty
+        _:test( "booneUnit.features not empty", function ()
+            local f = booneUnit.features
+            _:expect( #f ).is( 1 )
+            _:expect( f[1] ).isnt( nil )
+        end )
+        
+        _:test( ".currentFeature is nil again", function ()
+            local t = booneUnit
+            _:expect( t.currentFeature ).is( nil )
+        end )
+    end )
+    
+    -- Feature Properties --
+    _:describe( '"booneUnit:define()" results', function()
+        -- feature members 
+        local featureMembers = { description = "string", 
+                                 tests = "table", 
+                                 featureTests = "function", 
+                                 runTests = "function",
+                                 before = "function",
+                                 after = "function" }
+        memberTypeTest( "features[1]", booneUnit.features[1], featureMembers )
+        local featureValues = { description = featureDesc, 
+                                featureTests = featureFunc,
+                                runTests = booneUnit.newFeature.runTests,
+                                before = booneUnit.newFeature.before, 
+                                after = booneUnit.newFeature.after }
+        memberValueTest( "features[1]", booneUnit.features[1], featureValues )
+        -- feature description 
+        _:test( "feature description matches argument", function ()
+            local target = booneUnit.features[1]
+            _:expect( target.description ).is( featureDesc )
+        end )
+        
+        -- run feature tests 
+        _:test( "run feature tests", function ()
+            local target = booneUnit.features[1]
+            _:expect( target.featureTests ).isnt( nil )
+            _:expect( target.runTests ).isnt( nil )
+            _:expect( target:runTests() ).is( nil )
+        end )
+        --]]
+    end )
+end
+    
+function bestBooneUnitExpect()
     booneUnit:reset()
     _:describe( "Function booneUnit:expect() takes an argument and returns a set of functions which evaluate that argument.", function()
         _:test( "expect() throws error if not inside a test", function()
@@ -278,84 +327,51 @@ function testBooneUnitExpect()
         end
     end )
     
-end
-
-function bestMoonUnit() 
-    -- Feature Creation --
-    _:describe( "booneUnit creates Features", function ()
-        
-        _:before( function() end )
-        
-        -- booneUnit member test 
-        local unitMembers = { features = "table", 
-                              currentFeature = "nil", 
-                              currentTest = "nil", 
-                              describe = "function",
-                              test = "function",
-                              reset = "function",
-                              bork = "number",
-                              beck = "number" }
-        memberTypeTest( "booneUnit", booneUnit, unitMembers )
-        
-        -- features is empty
-        _:test( "booneUnit.features is empty", function ()
-            local f = booneUnit.features
-            _:expect( f[1] ).is( nil )
-            _:expect( #f ).is( 0 )
-        end )
-        
-        _:test( "booneUnit.currentFeature is nil", function ()
-            local f = booneUnit
-            _:expect( f.currentFeature ).is( nil )
-        end )
-        
-        --call :describe()
-        _:test( "call describe(), returns nil", function ()
-            _:expect( booneUnit:describe( featureDesc, featureFunc ) ).isnt( nil )
-        end )
-        
-        -- features not empty
-        _:test( "booneUnit.features not empty", function ()
-            local f = booneUnit.features
-            _:expect( #f ).is( 1 )
-            _:expect( f[1] ).isnt( nil )
-        end )
-        
-        _:test( ".currentFeature is nil again", function ()
-            local t = booneUnit
-            _:expect( t.currentFeature ).is( nil )
-        end )
+    _:describe( "booneUnit:expect( table ).has( value )", function()
     end )
     
-    -- Feature Properties --
-    _:describe( '"booneUnit:define()" results', function()
-        -- feature members 
-        local featureMembers = { description = "string", 
-                                 tests = "table", 
-                                 featureTests = "function", 
-                                 runTests = "function",
-                                 before = "function",
-                                 after = "function" }
-        memberTypeTest( "features[1]", booneUnit.features[1], featureMembers )
-        local featureValues = { description = featureDesc, 
-                                featureTests = featureFunc,
-                                runTests = booneUnit.newFeature.runTests,
-                                before = booneUnit.newFeature.before, 
-                                after = booneUnit.newFeature.after }
-        memberValueTest( "features[1]", booneUnit.features[1], featureValues )
-        -- feature description 
-        _:test( "feature description matches argument", function ()
-            local target = booneUnit.features[1]
-            _:expect( target.description ).is( featureDesc )
+end
+
+function testBooneUnitTest()
+    -- booneUnit.test()
+    _:describe( "booneUnit.test() returns a test object", function()
+        _:test( "booneUnit.test() exists", function()
+            _:expect( type( booneUnit.test ) ).is( "function" )
+        end )
+        -- test() returns a table
+        _:test( "booneUnit:test( description_string, function ) returns a table", function()
+            local testDesc = "Gingerbread Man"
+            local emptyTestFunc = function() end 
+            _:expect( type( booneUnit:test( testDesc, emptyTestFunc ) ) ).is( "table" )
+        end )
+        -- test() without arguments returns a table
+        _:test( "booneUnit:test() without arguments returns a table", function()
+            _:expect( type( booneUnit:test() ) ).is( "table" )
+        end )
+        -- properties of that table
+        do
+            local testDesc = "Gingerbread Man"
+            local emptyTestFunc = function() end 
+            local testTable = booneUnit:test( testDesc, emptyTestFunc )
+            local testTableProperties = { passed = "function",
+                                          report = "function",
+                                          results = "table",
+                                          description = "string",
+                                          registerResult = "function" }
+            memberTypeTest("booneUnit:test produces table", testTable, testTableProperties )
+        end
+        
+        -- length of table "results"
+        _:test( "length of table results is 0 with empty test", function()
+            local testDesc = "Gingerbread Man"
+            local emptyTestFunc = function() end 
+            local testTable = booneUnit:test( testDesc, emptyTestFunc )
+            _:expect( #testTable.results ).is( 0 )
         end )
         
-        -- run feature tests 
-        _:test( "run feature tests", function ()
-            local target = booneUnit.features[1]
-            _:expect( target.featureTests ).isnt( nil )
-            _:expect( target.runTests ).isnt( nil )
-            _:expect( target:runTests() ).is( nil )
-        end )
-        --]]
+        --test registerResult(result, actual, expected)
+        --appends entry to table test.results
+        
     end )
+    
 end
